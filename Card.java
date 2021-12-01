92574b6ceb7613c608c12b957726b16f605c0523
dualxyeray <94892642+dualxyeray@users.noreply.github.com>
2021-12-01 20:48:41 +0100

    Update Card.java

--- Card.java
+++ Card.java
@@ -1,10 +1,10 @@
 package ejercicio2;
 
 public class Card {
-
+//Esto es una cadena de caratres que te imprime los palos y el valor de las cartas//
 	public String suit;
 	public String value;
-	
+	//Es un metodo que te piede el valor y el palo de la carta.//
 	public Card (String suit, String value) {
 		this.suit = suit;
 		this.value = value;
